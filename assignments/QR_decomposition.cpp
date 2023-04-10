243de44e4e2862826b6b3a440c9383b403457c13
Fara_light <efanoffkirill@yandex.ru>
2023-04-10 23:43:10 +0300

    Assignment of finding eigen values of matrix

---
+++ assignments/QR_decomposition.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include </home/fara/numeric_methods/matrix.h>
+#include </home/fara/numeric_methods/matrix_operations.h>
+
+template <typename T>
+std::ostream& operator << (std::ostream& os, const std::complex<T>& val) {
+	T real = std::real(val);
+	T imag = std::imag(val);
+	os << real;
+	if (imag) {
+		std::cout << (imag < 0 ? " - " : " + ") << std::fabs(imag) << "i";
+	}
+	return os;
+}
+
+int main() {
+	std::cout << "Enter matrix order: ";
+	size_t n;
+	std::cin >> n;
+	long double EPS;
+	std::cout << "Enter EPS: ";
+	std::cin >> EPS;
+	Matrix<long double> matrix(n, n);
+	std::cout << "Enter matrix: " << std::endl;
+	for (size_t row = 0; row < n; ++row) {
+		for (size_t column = 0; column < n; ++column) {
+			std::cin >> matrix[row][column];
+		}
+	}	
+	auto ans = findMatrixEigenValuesQR(matrix, EPS);
+	std::cout << "Eigen values: " << std::endl;
+	for (const auto& eigenValue: ans) {
+		std::cout << eigenValue << std::endl;
+	}
+	std::cout << std::endl;
+	return 0;
+}
